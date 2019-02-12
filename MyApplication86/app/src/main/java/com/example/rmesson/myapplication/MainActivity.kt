e2357b1662921eafd47e3ad385d006fce6a3d44b
reymesson1 <reymesson1>
2019-02-11 20:13:01 -0400

    Downblocks

--- MyApplication86/app/src/main/java/com/example/rmesson/myapplication/MainActivity.kt
+++ MyApplication86/app/src/main/java/com/example/rmesson/myapplication/MainActivity.kt
@@ -6,15 +6,17 @@ import android.hardware.Sensor
 import android.hardware.SensorManager
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
-import android.util.Log
-import android.view.Menu
 import android.widget.LinearLayout
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import android.os.CountDownTimer
 
 class MainActivity : AppCompatActivity(), SensorEventListener{
 
     var move : Int = 100
+    var moveBlocks : Int = 100
+    var timeLeftInMilliseconds: Long = 600000 //10min
+    var timeRunning: Boolean = false
+    var countDownTimer : CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +27,56 @@ class MainActivity : AppCompatActivity(), SensorEventListener{
         var mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
         SM.registerListener(this,mySensor,SensorManager.SENSOR_DELAY_NORMAL)
+
+        startStop()
+
+    }
+
+    fun startStop(){
+
+        startTimer()
+    }
+
+    fun startTimer(){
+
+        countDownTimer = object: CountDownTimer(timeLeftInMilliseconds,100){
+            override fun onFinish() {
+
+            }
+
+            override fun onTick(l: Long) {
+
+                timeLeftInMilliseconds = l
+                var params = nave.getLayoutParams() as LinearLayout.LayoutParams
+
+                params.setMargins(0,moveBlocks,0, 0)
+
+                moveBlocks+=10
+
+                blocks1.layoutParams = params
+
+                updateTimer()
+            }
+        }.start()
+        timeRunning = true
+
+
+    }
+
+    fun updateTimer(){
+
+        var minutes = timeLeftInMilliseconds / 60000
+        var seconds = timeLeftInMilliseconds % 60000 / 1000
+
+        var timeLeftText: String
+
+        timeLeftText = "" + minutes
+        timeLeftText += ":"
+        if(seconds<10) timeLeftText += "0"
+        timeLeftText += seconds
+
+        textView3.setText(timeLeftText)
+
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
@@ -35,7 +87,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener{
 
         textView.text = event!!?.values[0].toString()
         textView2.text = event!!?.values[1].toString()
-        textView3.text = event!!?.values[2].toString()
+//        textView3.text = event!!?.values[2].toString()
 
 
         var params = nave.getLayoutParams() as LinearLayout.LayoutParams

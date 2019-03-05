4b38a58a9b95cc6408c60917e2e2daeff416629f
messore <rey.messon@verizon.com>
2019-03-05 19:51:19 -0400

    For tomorrow

---
+++ MyApplication117/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.myapplication
+
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.activityUiThread
+import org.jetbrains.anko.doAsync
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    var moveOne : Int = 0
+    var moveTwo : Int = 0
+    var moveThree : Int = 0
+    var moveFour : Int = 0
+    var moveNave : Int = 0
+    var trial : Int = 0
+    var moveBullet : Int = 0
+    var actualPosition : Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        doAsync {
+
+            for(i in 1..2000){
+
+                Thread.sleep(2000)
+
+                activityUiThread {
+
+                    var paramsOne = blocks1.getLayoutParams() as LinearLayout.LayoutParams
+                    paramsOne.setMargins(0,moveOne,0, 0)
+                    blocks1.layoutParams = paramsOne
+
+                    var paramsTwo = blocks2.getLayoutParams() as LinearLayout.LayoutParams
+                    paramsTwo.setMargins(0,moveTwo,0, 0)
+                    blocks2.layoutParams = paramsTwo
+
+                    var paramsThree = blocks3.getLayoutParams() as LinearLayout.LayoutParams
+                    paramsThree.setMargins(0,moveThree,0, 0)
+                    blocks3.layoutParams = paramsThree
+
+                    var paramsFour = blocks4.getLayoutParams() as LinearLayout.LayoutParams
+                    paramsFour.setMargins(0,moveFour,0, 0)
+                    blocks4.layoutParams = paramsFour
+
+                    moveOne +=100
+                    moveTwo +=10
+                    moveThree +=50
+                    moveFour +=70
+
+                    if(moveOne==900) {   moveOne = 0 }
+                    if(moveTwo==900) {   moveTwo = 0 }
+                    if(moveThree==900) {   moveThree = 0 }
+                    if(moveFour==900) {   moveFour = 0 }
+
+
+                }
+            }
+        }
+
+
+        
+
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+
+        var params = nave.layoutParams!! as FrameLayout.LayoutParams
+
+        if(event!!?.values[0]>2.8 && event.values[0]<9.0) {
+
+            params.setMargins(0,moveNave,0, 0)
+
+            nave.layoutParams = params
+
+            moveNave-=20
+
+        }
+
+        if(event!!?.values[0]>-9.8 && event.values[0]<-2.8){
+
+            params.setMargins(0,moveNave,0, 0)
+
+            nave.layoutParams = params
+
+            moveNave += 20
+        }
+
+
+
+    }
+
+
+
+}
